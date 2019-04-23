efcbb403da1ea4ce67a18b8ed445c190421c3ba7
Mikael Hallendal <hallski@hallski.org>
2019-04-23 21:57:31 +0200

    Supports adding and removing windows from layout

--- src/layout.test.ts
+++ src/layout.test.ts
@@ -1,45 +1,67 @@
-import { create, addWindow, removeWindow, zeroRect } from "./layout"
+import { create, addWindow, removeWindow } from "./layout"
 
 describe("Layout", () => {
   it("Should have a root node", () => {
-    let layout = create(zeroRect)
+    let layout = create()
 
-    expect(layout.root.kind).toEqual("Empty")
+    expect(layout.root.debugName()).toEqual("Empty")
   })
 
   it("should insert a single window as a 'monocle' window", () => {
-    let layout = create(zeroRect)
+    let layout = create()
 
     layout = addWindow(layout, 5)
 
-    expect(layout.root.kind).toEqual("Monocle")
+    expect(layout.root.debugName()).toEqual("<5>")
   })
 
   it("should create a 'binary' group when second window is inserted", () => {
-    let layout = create(zeroRect)
+    let layout = create()
 
     layout = addWindow(layout, 4)
     layout = addWindow(layout, 7)
 
-    expect(layout.root.kind).toEqual("Binary")
+    expect(layout.root.debugName()).toEqual("<(4,7)>")
   })
 
   it("should support removing a 'monocle' window", () => {
-    let layout = create(zeroRect)
+    let layout = create()
 
     layout = addWindow(layout, 5)
     layout = removeWindow(layout, 5)
 
-    expect(layout.root.kind).toEqual("Empty")
+    expect(layout.root.debugName()).toEqual("Empty")
   })
 
   it("should support removing a window from a group", () => {
-    let layout = create(zeroRect)
+    let layout = create()
 
     layout = addWindow(layout, 3)
     layout = addWindow(layout, 4)
     layout = removeWindow(layout, 3)
 
-    expect(layout.root.kind).toEqual("Monocle")
+    expect(layout.root.debugName()).toEqual("<4>")
+  })
+
+  it("should support adding three windows", () => {
+    let layout = create()
+
+    layout = addWindow(layout, 4)
+    layout = addWindow(layout, 5)
+    layout = addWindow(layout, 6)
+
+    expect(layout.root.debugName()).toEqual("<(4,(5,6))>")
+  })
+
+  it("should support removing a child one level down", () => {
+    let layout = create()
+
+    layout = addWindow(layout, 5)
+    layout = addWindow(layout, 6)
+    layout = addWindow(layout, 7)
+
+    layout = removeWindow(layout, 7)
+
+    expect(layout.root.debugName()).toEqual("<(5,6)>")
   })
 })
--- src/layout.ts
+++ src/layout.ts
@@ -1,137 +1,139 @@
 type ID = number
 
-interface Window {
-  kind: "Window"
+interface Node {
+  addWindow(window: Window): Node
+  removeWindow(window: Window): Node | undefined
+
+  debugName(): string
+}
+
+class Window implements Node {
   id: ID
+
+  constructor(id: ID) {
+    this.id = id
+  }
+
+  addWindow(window: Window): Node {
+    return new BinaryGroup(this, window)
+  }
+
+  removeWindow(window: Window): Node | undefined {
+    if (this.id === window.id) {
+      return undefined
+    }
+
+    return this
+  }
+
+  debugName(): string {
+    return `${this.id}`
+  }
 }
 
-interface EmptyGroup {
-  kind: "Empty"
+class BinaryGroup implements Node {
+  private left: Node
+  private right?: Node
+
+  constructor(left: Node, right?: Node) {
+    this.left = left
+    this.right = right
+  }
+
+  addWindow(window: Window): Node {
+    if (!this.right) {
+      return new BinaryGroup(this.left, window)
+    }
+
+    return new BinaryGroup(this.left, this.right.addWindow(window))
+  }
+
+  removeWindow(window: Window): Node | undefined {
+    if (this.left instanceof Window && this.left.id === window.id) {
+      return this.right
+    }
+
+    if (this.right instanceof Window && this.right.id === window.id) {
+      return this.left
+    }
+
+    const newLeft = this.left && this.left.removeWindow(window)
+    const newRight = this.right && this.right.removeWindow(window)
+
+    if (!newLeft && !newRight) {
+      return undefined
+    }
+
+    if (newLeft && newRight) {
+      return new BinaryGroup(newLeft, newRight)
+    }
+
+    return newLeft ? newLeft : newRight
+  }
+
+  debugName(): string {
+    if (!this.left) {
+      return "Empty"
+    }
+    if (!this.right) {
+      return `(${this.left.debugName()})`
+    }
+    return `(${this.left.debugName()},${this.right.debugName()})`
+  }
 }
 
-interface MonocleGroup {
-  kind: "Monocle"
-  window: Window
+class Root {
+  private child?: Node
+
+  constructor(child?: Node) {
+    this.child = child
+  }
+
+  addWindow(window: Window): Root {
+    if (this.child) {
+      return new Root(this.child.addWindow(window))
+    }
+
+    return new Root(window)
+  }
+
+  removeWindow(window: Window): Root {
+    if (!this.child) {
+      return new Root()
+    }
+
+    return new Root(this.child.removeWindow(window))
+  }
+
+  debugName(): string {
+    if (!this.child) {
+      return "Empty"
+    } else {
+      return `<${this.child.debugName()}>`
+    }
+  }
 }
 
-interface BinaryGroup {
-  kind: "Binary"
-  left: Node
-  right: Node
-}
-
-type Group = EmptyGroup | MonocleGroup | BinaryGroup
-
-type Node = Window | Group
+//type Group = EmptyGroup | MonocleGroup | BinaryGroup
 
 interface Layout {
-  root: Group
+  root: Root
 }
 
 export function create(): Layout {
-  return { root: createEmptyGroup() }
-}
-
-function createEmptyGroup(): Group {
-  return {
-    kind: "Empty"
-  }
-}
-
-function createMonocleGroup(windowID: ID): Group {
-  return {
-    kind: "Monocle",
-    window: createWindow(windowID)
-  }
-}
-function createBinaryGroup(left: Node, right: Node): Group {
-  return {
-    kind: "Binary",
-    left,
-    right
-  }
-}
-
-function createWindow(id: ID): Window {
-  return {
-    kind: "Window",
-    id
-  }
-}
-
-function addWindowToNode(node: Node, windowID: ID): Group {
-  switch (node.kind) {
-    case "Window":
-      return createBinaryGroup(node, createWindow(windowID))
-    default:
-      return addWindowInGroup(node, windowID)
-  }
-}
-
-function addWindowInGroup(group: Group, windowID: ID): Group {
-  switch (group.kind) {
-    case "Empty":
-      return createMonocleGroup(windowID)
-    case "Monocle":
-      return createBinaryGroup(group.window, createWindow(windowID))
-    case "Binary":
-      return createBinaryGroup(
-        group.left,
-        addWindowToNode(group.right, windowID)
-      )
-  }
-}
-
-function removeWindowFromGroup(group: Group, windowID: ID): Group {
-  switch (group.kind) {
-    case "Empty":
-      return group // Window ID does not exist
-    case "Monocle":
-      if (group.window.id === windowID) {
-        return createEmptyGroup()
-      } else {
-        return group // Window ID does not exist
-      }
-    case "Binary":
-      if (group.left.kind === "Window" && group.left.id === windowID) {
-        if (group.right.kind === "Window") {
-          return createMonocleGroup(group.right.id)
-        } else {
-          return group.right
-        }
-      } else if (group.right.kind === "Window" && group.right.id === windowID) {
-        if (group.left.kind === "Window") {
-          return createMonocleGroup(group.left.id)
-        } else {
-          return group.left
-        }
-      } else {
-        return group // TODO: Need to handle other cases here
-      }
-  }
+  return { root: new Root() }
 }
 
 export function addWindow(layout: Layout, id: ID): Layout {
   return {
     ...layout,
-    root: addWindowInGroup(layout.root, id)
+    root: layout.root.addWindow(new Window(id))
   }
 }
 
 export function removeWindow(layout: Layout, windowID: ID): Layout {
   return {
     ...layout,
-    root: removeWindowFromGroup(layout.root, windowID)
+    root: layout.root.removeWindow(new Window(windowID))
   }
-}
-
-// Return a list of layout changes that should be applied
-//export function diff(layout1: Layout, layout2: Layout) {}
-
-export function isWindow(node: Node): node is Window {
-  return (node as Window).kind === "Window"
-}
-
-export function isGroup(node: Node): node is Group {
-  return !isWindow(node)
 }
